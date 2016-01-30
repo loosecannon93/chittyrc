1ae294e1d81c72225df99f9a87da76b47cd1af59
Cannon Matthews <cannon@cannonmatthews.com>
2016-01-30 16:52:42 -0500

    working main function with command line args and logging

--- lib/chilog/chilog.go
+++ lib/chilog/chilog.go
@@ -15,90 +15,72 @@
 
 package chilog
 
-import ( 
-    "log"
-    "io"
-    "io/ioutil"
-    "os"
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 /* Log levels */
 type LogLevel int
+
 const (
-    QUIET    LogLevel = 10 * iota
-    CRITICAL LogLevel = 10 * iota
-    ERROR    LogLevel = 10 * iota
-    WARNING  LogLevel = 10 * iota
-    INFO     LogLevel = 10 * iota
-    DEBUG    LogLevel = 10 * iota
-    TRACE    LogLevel = 10 * iota
+	QUIET    LogLevel = 10 * iota
+	CRITICAL LogLevel = 10 * iota
+	ERROR    LogLevel = 10 * iota
+	WARNING  LogLevel = 10 * iota
+	INFO     LogLevel = 10 * iota
+	DEBUG    LogLevel = 10 * iota
+	TRACE    LogLevel = 10 * iota
 )
 
 /* map levels to prefix strings */
 var prefixes = map[LogLevel]string{
-    QUIET   :"",
-    CRITICAL:"CRITICAL: ",
-    ERROR   :"ERROR: ",
-    WARNING :"WARNING: ",
-    INFO    :"INFO: ",
-    DEBUG   :"DEBUG: ",
-    TRACE   :"TRACE: ",
+	QUIET:    "",
+	CRITICAL: "CRITICAL: ",
+	ERROR:    "ERROR: ",
+	WARNING:  "WARNING: ",
+	INFO:     "INFO: ",
+	DEBUG:    "DEBUG: ",
+	TRACE:    "TRACE: ",
 }
 
-/* Actual Logger lobjects to log to. 
+/* Actual Logger lobjects to log to.
  * Init initializes these, setting those below the log level
- * to discard their inputs using ioutil.DIscard 
-*/
-var loggers = map[LogLevel](*log.Logger){
-    QUIET   :nil,
-    CRITICAL:nil,
-    ERROR   :nil,
-    WARNING :nil,
-    INFO    :nil,
-    DEBUG   :nil,
-    TRACE   :nil,
-}
-
-
-
-func Init(level LogLevel) { 
-    /* Lambda to determine what the output is */ 
-    output := func(l LogLevel ) io.Writer { 
-      if l > level { 
-        return ioutil.Discard 
-      } else { 
-        return os.Stdout 
-      } 
-    } 
-
-    for l := range loggers { 
-      loggers[l] = log.New(output(l), prefixes[l],
-        log.Ldate|log.Ltime|log.Lshortfile)
-    }
-}
-  
-
-/*
- * Log - Print a log message
- *
- * level: Logging level of the message
- *
- * ...: Extra parameters to pass to Println
- *
- * Returns: nothing.
+ * to discard their inputs using ioutil.DIscard
  */
-func Log(level LogLevel, a...interface{}) { 
-  loggers[level].Println(a...)
+var (
+	Critical *log.Logger
+	Error    *log.Logger
+	Warning  *log.Logger
+	Info     *log.Logger
+	Debug    *log.Logger
+	Trace    *log.Logger
+)
+
+func Init(level LogLevel) {
+	/* Lambda to determine what the output is */
+	output := func(l LogLevel) io.Writer {
+		if l > level {
+			return ioutil.Discard
+		} else {
+			return os.Stdout
+		}
+	}
+
+	Critical = log.New(output(CRITICAL), prefixes[CRITICAL],
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(output(ERROR), prefixes[ERROR],
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(output(WARNING), prefixes[WARNING],
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(output(INFO), prefixes[INFO],
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Debug = log.New(output(DEBUG), prefixes[DEBUG],
+		log.Ldate|log.Ltime|log.Lshortfile)
+	Trace = log.New(output(TRACE), prefixes[TRACE],
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info.Print("Initialized Logging system with level ", prefixes[level])
 }
-
-/* Logf - Print a fomatted message
- * 
- * Same as Log, but takes a format string and passes to 
- * Printf rather than Println
- */
-func Logf(level LogLevel, LogLevel, format string, a...interface{}) {
-  loggers[level].Printf(format, a...)
-}
-  
-
-
--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
-import(
-  "github.com/loosecannon93/chittyrc/lib/chilog"
+import (
+	"flag"
+	"github.com/loosecannon93/chittyrc/lib/chilog"
 )
 
 func main() {
-  chilog.Init(chilog.INFO)
-  chilog.Log(chilog.WARNING, "wow such logging")
+
+	// Parse Command line flags
+	var (
+		oper_password string
+		port          int
+		verbose       bool
+		vverbose      bool
+		quiet         bool
+	)
+
+	flag.StringVar(&oper_password, "o", "pass", "Operator Password")
+	flag.BoolVar(&verbose, "v", false, "Verbose: log upto DEBUG level")
+	flag.BoolVar(&vverbose, "vv", false, "Very Verbose: log everything including TRACE")
+	flag.BoolVar(&quiet, "q", false, "Print nothing to the log")
+	flag.IntVar(&port, "p", 6667, "Port to bind to")
+
+	flag.Parse()
+
+	// Set the log level
+	log_level := chilog.INFO
+
+	switch {
+	case quiet:
+		log_level = chilog.QUIET
+	case vverbose:
+		log_level = chilog.TRACE
+	case verbose:
+		log_level = chilog.DEBUG
+	}
+	chilog.Init(log_level)
+
+	chilog.Info.Println("Server Starting")
+	chilog.Info.Print("Operator password is '", oper_password, "'")
+	chilog.Info.Println("port is", port)
+	chilog.Debug.Println("Debugging test")
+
 }
